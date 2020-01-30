35d520de80fb058531ba479dca93bc432c4b7785
SF2005 <1208733578@qq.com>
2020-01-31 01:31:51 +0800

    audio:add exthm_audio.mk for audio of exthm

--- config/common.mk
+++ config/common.mk
@@ -107,6 +107,9 @@ include vendor/lineage/config/aosp_audio.mk
 # Include Lineage audio files
 include vendor/lineage/config/lineage_audio.mk
 
+# Include exTHm audio files
+include vendor/lineage/config/exthm_audio.mk
+
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
 include vendor/lineage/config/lineage_sdk_common.mk
---
+++ config/exthm_audio.mk
@@ -0,0 +1,20 @@
+#
+# exTHm Audio Files
+#
+
+ALARM_PATH := vendor/lineage/prebuilt/common/media/audio/alarms
+NOTIFICATION_PATH := vendor/lineage/prebuilt/common/media/audio/notifications
+RINGTONE_PATH := vendor/lineage/prebuilt/common/media/audio/ringtones
+
+# Alarms
+#PRODUCT_COPY_FILES += 
+
+# Notifications
+#PRODUCT_COPY_FILES += 
+
+# Ringtones
+ifeq ($(TARGET_NEEDS_BOOSTED_SOUNDS),true)
+#PRODUCT_COPY_FILES += 
+else
+#PRODUCT_COPY_FILES += 
+endif
